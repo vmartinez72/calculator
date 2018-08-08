358514fce52ea7354a3ca1183fb48ca0fa25fb4e
vm <vmartinez72.vm@gmail.com>
2018-08-08 16:42:25 -0700

    add output box and js to file.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,22 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var subtract = function(number1, number2) {
+  return number1 + number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 + number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 + number2;
+};
+
+$(document).ready(function(){
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+
+
+  alert(add(number1, number 2));
+});
